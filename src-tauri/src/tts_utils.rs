ee7f176025f4284c03f7f795b0ecf70254d084e4
Avery Schreiner <schreineravery@gmail.com>
2024-01-22 14:52:57 -0600

    static tts

--- src-tauri/src/tts_utils.rs
+++ src-tauri/src/tts_utils.rs
@@ -1,21 +1,33 @@
-use tts::Tts;
+use lazy_static::lazy_static;
 use std::thread;
+use tts::Error;
+use tts::Tts;
+
+lazy_static! {
+    pub static ref TTS_INSTANCE: Result<Tts, Error> = Tts::default();
+}
 
 pub fn speak(message: String) {
-    thread::spawn(move || {
-        let mut tts = Tts::default().unwrap();
-        let voices = tts.voices().unwrap();
+    match TTS_INSTANCE.as_ref() {
+        Ok(tts) => {
+            //since we can't make a borrowed value mutable we have to clone it
+            let mut cloned_tts = tts.clone();
+            thread::spawn(move || {
+                let voices = cloned_tts.voices().unwrap();
 
-        #[cfg(target_os = "macos")]
-        let _ = tts.set_voice(&voices[33]);
-        #[cfg(target_os = "windows")]
-        let _ = tts.set_voice(&voices[2]);
+                #[cfg(target_os = "macos")]
+                let _ = cloned_tts.set_voice(&voices[33]);
 
-        tts.speak(message, true).unwrap();
+                #[cfg(target_os = "windows")]
+                let _ = cloned_tts.set_voice(&voices[2]);
 
-        while tts.is_speaking().unwrap() {
-            println!("tts is speaking!");
-            std::thread::sleep(std::time::Duration::from_secs(1));
-        }
-    });
+                cloned_tts.speak(message, true).unwrap();
+
+                while tts.is_speaking().unwrap() {
+                    std::thread::sleep(std::time::Duration::from_secs(1));
+                }
+            });
+        },
+        Err(e) => panic!("Failed to get static tts ref! {e}")
+    }
 }
