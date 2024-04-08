ad12b3b608846561421881794fa5c77e7784591d
Parker W Christy <pchristy03@gmail.com>
2024-04-07 21:11:31 -0500

    finally fix it

--- src-tauri/src/audio_output.rs
+++ src-tauri/src/audio_output.rs
@@ -1,8 +1,16 @@
 use crate::assistant;
-use crossbeam::channel::{bounded, Receiver, Sender};
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use cpal::{Device, Sample, StreamError, FromSample};
-use std::{collections::VecDeque, error::Error, sync::{Arc, Mutex}, thread, time::Duration};
+use cpal::{
+    Device, FromSample, Sample, SampleFormat, StreamConfig, StreamError, SupportedStreamConfig,
+};
+use crossbeam::channel::{bounded, Receiver, Sender};
+use std::{
+    collections::VecDeque,
+    error::Error,
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
 
 pub fn get_audio_output_device() -> Device {
     let host = cpal::default_host();
@@ -10,17 +18,25 @@ pub fn get_audio_output_device() -> Device {
     let audio_output_device: Device = loop {
         match host.default_output_device() {
             Some(device) => break device,
-            None => thread::sleep(Duration::from_secs(1))
+            None => thread::sleep(Duration::from_secs(1)),
         }
         println!("Looking for output device.")
     };
-    println!("Found output device! -> {:?}", audio_output_device.name().unwrap());
+    println!(
+        "Found output device! -> {:?}",
+        audio_output_device.name().unwrap()
+    );
 
     audio_output_device
 }
 
-pub fn run_stream(audio_output_receiver: Receiver<Vec<i16>>, device: Device, synthesizing: Arc<Mutex<bool>>) -> Result<(), Box<dyn Error>> {
-    let config = device.default_output_config().unwrap();
+pub fn run_stream(
+    audio_output_receiver: Receiver<Vec<i16>>,
+    device: Device,
+    synthesizing: Arc<Mutex<bool>>,
+) -> Result<(), Box<dyn Error>> {
+    let format = device.default_input_config().unwrap().sample_format();
+    let mut config: StreamConfig = device.default_output_config().unwrap().into();
     let (error_sender, error_receiver): (Sender<StreamError>, Receiver<StreamError>) = bounded(1);
 
     fn error_callback(e: StreamError, error_sender: Sender<StreamError>) {
@@ -28,12 +44,11 @@ pub fn run_stream(audio_output_receiver: Receiver<Vec<i16>>, device: Device, syn
     }
 
     fn write_audio<T>(output: &mut [T], audio_output_receiver: Receiver<Vec<i16>>)
-    where 
-        T: FromSample<i16> + Sample
+    where
+        T: FromSample<i16> + Sample,
     {
         if let Ok(samples) = audio_output_receiver.try_recv() {
             let mut samples: VecDeque<T> = samples.into_iter().map(T::from_sample).collect();
-
             for frame in output.chunks_mut(2) {
                 for sample in frame.iter_mut() {
                     *sample = samples.pop_front().unwrap_or(T::EQUILIBRIUM);
@@ -44,63 +59,95 @@ pub fn run_stream(audio_output_receiver: Receiver<Vec<i16>>, device: Device, syn
 
     let audio_output_receiver_clone = audio_output_receiver.clone();
 
-    let stream = match config.sample_format() {
+    let mut smples: VecDeque<f32> = VecDeque::new();
+
+    #[cfg(target_os = "macos")]
+    {
+        config = StreamConfig {
+            channels: 2,
+            sample_rate: cpal::SampleRate(45000),
+            buffer_size: cpal::BufferSize::Default,
+        };
+    }
+
+    let stream = match format {
         cpal::SampleFormat::F32 => device.build_output_stream(
-        &config.clone().into(),
-        move |data: &mut [f32], _| write_audio(data, audio_output_receiver.clone()),
-        move |e| error_callback(e, error_sender.clone()),
-        None
+            &config.clone().into(),
+            // move |data: &mut [f32], _| {
+            move |data: &mut [f32], _| write_audio(data, audio_output_receiver.clone()),
+            //     if let Ok(samples) = audio_output_receiver.try_recv() {
+            //         smples.extend(samples.into_iter().map(f32::from_sample));
+            //     }
+            //     for chunk in data.chunks_mut(2) {
+            //         for sample in chunk.iter_mut() {
+            //             *sample = smples.pop_front().unwrap_or(0.0);
+            //         }
+            //     }
+            // },
+            move |e| error_callback(e, error_sender.clone()),
+            None,
         ),
         cpal::SampleFormat::I16 => device.build_output_stream(
             &config.clone().into(),
             move |data: &mut [i16], _| write_audio(data, audio_output_receiver.clone()),
             move |e| error_callback(e, error_sender.clone()),
-            None
+            None,
         ),
         cpal::SampleFormat::U16 => device.build_output_stream(
             &config.clone().into(),
             move |data: &mut [u16], _| write_audio(data, audio_output_receiver.clone()),
             move |e| error_callback(e, error_sender.clone()),
-            None
+            None,
         ),
-        _ => panic!()
-    }.expect("Failed to build audio output stream!");
+        _ => panic!(),
+    }
+    .expect("Failed to build audio output stream!");
 
     match stream.play() {
         Ok(_) => println!("Successfully started audio output stream!"),
-        Err(error) => println!("Failed to start audio output stream: {}", error)
+        Err(error) => println!("Failed to start audio output stream: {}", error),
     }
 
     loop {
         if let Ok(stream_error) = error_receiver.try_recv() {
             drop(stream);
-            return Err(Box::new(stream_error))
-        }
-        else if !*synthesizing.lock().unwrap() && audio_output_receiver_clone.is_empty() {
-            return Ok(())
+            return Err(Box::new(stream_error));
+        } else if !*synthesizing.lock().unwrap() && audio_output_receiver_clone.is_empty() {
+            return Ok(());
         }
     }
 }
 
 pub async fn speak(assistant_message: String) -> Result<(), Box<dyn Error>> {
     // create speech sender and receiver
-    let (audio_output_sender, audio_output_receiver): (Sender<Vec<i16>>, Receiver<Vec<i16>>) = crossbeam::channel::unbounded::<Vec<i16>>();
+    let (audio_output_sender, audio_output_receiver): (Sender<Vec<i16>>, Receiver<Vec<i16>>) =
+        crossbeam::channel::unbounded::<Vec<i16>>();
     let synthesizing: Arc<Mutex<bool>> = Arc::new(Mutex::new(false));
 
     // find an output device
     let audio_output_device = get_audio_output_device();
-    let audio_output_config = audio_output_device.default_output_config().unwrap();    
+    let audio_output_config = audio_output_device.default_output_config().unwrap();
 
     // spawn create_speech with sender
     *synthesizing.lock().unwrap() = true;
     let create_speech_handle = tauri::async_runtime::spawn(async move {
-        assistant::create_speech(assistant_message, audio_output_sender, audio_output_config.sample_rate(), audio_output_config.channels()).await
+        assistant::create_speech(
+            assistant_message,
+            audio_output_sender,
+            audio_output_config.sample_rate(),
+            audio_output_config.channels(),
+        )
+        .await
     });
 
     // spawn output with receiver
     let synthesizing_clone = synthesizing.clone();
     let output_stream_handle = thread::spawn(move || {
-        let _ = run_stream(audio_output_receiver, audio_output_device, synthesizing_clone);
+        let _ = run_stream(
+            audio_output_receiver,
+            audio_output_device,
+            synthesizing_clone,
+        );
     });
 
     // wait for the threads to finish
@@ -108,5 +155,5 @@ pub async fn speak(assistant_message: String) -> Result<(), Box<dyn Error>> {
     *synthesizing.lock().unwrap() = false;
     output_stream_handle.join().unwrap();
 
-    return Ok(())
+    return Ok(());
 }
